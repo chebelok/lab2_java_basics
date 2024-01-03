7c8ae318153840a0f24bb030b962352c1421d522
chebelok <lotokhinivan@gmail.com>
2024-01-03 14:09:40 +0200

    added realisation for min and max sum for columns

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,18 +7,22 @@ public class Main {
         int C11 = 1316 % 11; // Обчислити суму найбільших елементів в стовпцях матриці з
         // непарними номерами та найменших елементів в стовпцях матриці з парними номерами
         double[][] matrix = {
-                {1,2,3,4,5},
-                {6,7,8,9,10},
+                {1, 2, 3, 4, 5},
+                {6, 7, 8, 9, 10},
                 {11,12,13,14,15},
                 {16,17,18,19,20},
                 {21,22,23,24,25}
         };
+        double num = matrix[2][0];
+        System.out.println(num);
         double[][] transM;
         Matrix mat = new Matrix();
         mat.showMatrix(matrix);
-        transM =  mat.transpposedMatrix(matrix);
+        transM =  mat.transposedMatrix(matrix);
         System.out.println("transposed matrix");
         mat.showMatrix(transM);
         System.out.println("Hello world!");
+        System.out.println(mat.oddColsMax(transM));
+        System.out.println(mat.evenColsMin(transM));
     }
 }
--- src/main/java/org/example/Matrix.java
+++ src/main/java/org/example/Matrix.java
@@ -9,7 +9,7 @@ public class Matrix {
             System.out.println();
         }
     }
-    public double[][] transpposedMatrix(double[][]matrix){
+    public double[][] transposedMatrix(double[][]matrix){
         int rows = matrix[0].length;
         int cols  = matrix.length;
 
@@ -21,4 +21,50 @@ public class Matrix {
         }
         return transMatrix;
     }
+
+    public double oddColsMax(double[][] matrix){
+        try {
+            int columns = matrix[0].length;
+            double sum = 0;
+
+            for (int j = 0; j < columns; j += 2) {
+                double max = matrix[0][j];
+                for (int i = 1; i < matrix.length; i++) {
+                    if (matrix[i][j] > max) {
+                        max = matrix[i][j];
+                    }
+                }
+                sum += max;
+            }
+
+            return sum;
+        } catch (Exception e) {
+            System.err.println("Помилка при обчисленнi суми найбiльших елементiв: " + e.getMessage());
+            return -1;
+        }
+
+    }
+    public double evenColsMin(double[][] matrix){
+        try {
+            int columns = matrix[0].length;
+            double sum = 0;
+
+            for (int j = 1; j < columns; j += 2) {
+                double min = matrix[0][j];
+                for (int i = 1; i < matrix.length; i++) {
+                    if (matrix[i][j] < min) {
+                        min = matrix[i][j];
+                    }
+                }
+                sum += min;
+            }
+
+            return sum;
+        } catch (Exception e) {
+            System.err.println("Помилка при обчисленнi суми найбiльших елементiв: " + e.getMessage());
+            return -1;
+        }
+
+    }
+
 }
